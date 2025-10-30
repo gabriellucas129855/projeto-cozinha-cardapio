6ce0ed95b74012215740a7ef64632846ea36f2ac
gabriellucas129855 <gabriel.9313373@aluno.mg.gov.br>
2025-10-30 14:31:47 -0300

    Gerenciar usuario

---
+++ script.js
@@ -0,0 +1,27 @@
+async function loginCoziheira(email,senha){
+    
+} 
+
+async function cadastrarCozinheira(nome,email,senha) {
+    
+}
+async function recuperarSenha(email) {
+    
+} 
+async function cadastrarCardapio(cardapio) {
+    
+}
+async function alterarCardapio(id,atualizarCardapio){}
+
+async function excluirCardapio(id) {
+    
+}
+async function buscarCardapio(id) {
+    
+}
+function exibirTabelaCardapio(cardapios){
+
+}
+async function listarCardapio() {
+    
+}
